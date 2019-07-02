cf044513d029afd8a185822404766a3385ff1bbb
Andrey Prisniak <aprisniak@lectrum.io>
2019-07-02 15:19:34 +0300

    fix(source): fix auth

--- source/routers/auth/index.js
+++ source/routers/auth/index.js
@@ -7,7 +7,7 @@ import { limiter } from '../../utils';
 
 export const router = express.Router();
 
-router.post('/', [ limiter(5, 60 * 1000) ], login);
-router.post('/', [ limiter(5, 60 * 1000) ], logout);
+router.post('/login', [ limiter(5, 60 * 1000) ], login);
+router.post('/logout', [ limiter(5, 60 * 1000) ], logout);
 
 export { router as auth };
